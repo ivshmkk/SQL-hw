b59503b80ee2464c612f8b43d4202918984f9cc9
Ivan Shmikk <shmikk.v@yandex.ru>
2024-06-17 16:19:28 +0700

    Modified SELECT 2, 3

--- select_z2, 3.sql
+++ select_z2, 3.sql
@@ -8,11 +8,17 @@ SELECT collection_title, year_of_release from collection
 WHERE year_of_release BETWEEN '2018-01-01' AND '2020-01-01';
 
 SELECT name_surname FROM musician
-WHERE name_surname LIKE '%';
+WHERE name_surname ILIKE '%';
 
 SELECT Track_title FROM track
-WHERE track_title LIKE '%my%'
-	OR track_title LIKE '%мой%';
+WHERE track_title ILIKE 'my'
+OR track_title ILIKE '% my'
+OR track_title ILIKE '% my %'
+OR track_title ILIKE 'my %'
+OR track_title ILIKE 'мой'
+OR track_title ILIKE '% мой'
+OR track_title ILIKE '% мой %'
+OR track_title ILIKE 'мой %';
 
 
 
@@ -20,10 +26,9 @@ WHERE track_title LIKE '%my%'
 SELECT Genre_id, COUNT(DISTINCT Musician_id) AS Number_of_musicians FROM Genre_musician
 GROUP BY Genre_id;
 
-SELECT Album_title, year_of_release, count(Track_title) AS tracks_in_albums FROM album
-JOIN Track ON Track.Album_id = Album.album_id
-GROUP BY Album_title, Album.Year_of_release
-HAVING Album.Year_of_release BETWEEN '2019-01-01' AND '2020-01-01';
+SELECT count(Track_id) AS tracks_in_albums FROM Track
+JOIN Album ON Track.Album_id = Album.album_id
+WHERE Album.Year_of_release BETWEEN '2019-01-01' AND '2020-12-31';
 
 SELECT Album_title, AVG(track.Track_duration) AS Tr_dur FROM Album
 JOIN Track ON Track.album_id = Album.album_id 
@@ -34,12 +39,10 @@ WHERE Name_surname NOT IN
 (SELECT Name_surname FROM musician
 JOIN Musician_album ON Musician.Musician_id = Musician_album.Musician_id 
 JOIN Album ON Album.album_id = Musician_album.album_id
-WHERE (SELECT EXTRACT(YEAR FROM year_of_release) FROM Album LIMIT 1) = 2020)
+WHERE DATE_PART('YEAR', Album.year_of_release::date) = 2020)
 GROUP BY Name_surname;
 
-SELECT Collection_title FROM collection
-WHERE Collection_title IN 
-(
+
 SELECT Collection_title FROM Collection
 JOIN Track_collection ON Collection.Collection_id = Track_collection.Collection_id
 JOIN Track ON Track.Track_id = Track_collection.Track_id
@@ -47,7 +50,6 @@ JOIN Album ON Album.Album_id = Track.Album_id
 JOIN Musician_album ON Musician_album.Album_id = Album.album_id
 JOIN Musician ON Musician.musician_id = Musician_album.musician_id
 WHERE Musician.Musician_id = 1
-)
 ORDER BY Collection_title;
 
 
